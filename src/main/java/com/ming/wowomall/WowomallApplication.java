f00b877db29fb3bd9f93a3249f38090d99938f54
MingX1 <969130721@qq.com>
2018-05-27 11:22:04 +0800

    first commit

---
+++ src/main/java/com/ming/wowomall/WowomallApplication.java
@@ -0,0 +1,12 @@
+package com.ming.wowomall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowomallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WowomallApplication.class, args);
+    }
+}
